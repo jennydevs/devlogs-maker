a87da16ca2ca2fbef6eaab6c1e0b5bf9972004b2
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2026-03-20 19:06:41 -0400

    move around methods and add labels

--- scripts/images.gd
+++ scripts/images.gd
@@ -1,11 +1,51 @@
 extends MarginContainer
 
 
-signal connect_startup(component: String);
+# =====================
+# ======= Nodes =======
+# =====================
 
+@onready var img_list = $Scroll/VBox;
+
+# =====================
+# ====== Signals ======
+# =====================
+
+signal connect_startup(component: String);
 signal create_notif_popup(msg);
 signal create_action_popup(msg, button_info, action);
 
+# ============================
+# ====== Signal Methods ======
+# ============================
+
+func _on_delete_button_pressed(image_item, img_path: String):
+	create_action_popup.emit(
+		"Are you sure you want to delete this image?",
+		{ 'yes': "Delete Image", 'no': "Cancel" },
+		_on_serious_delete_button_pressed.bind(image_item, img_path) 
+	);
+
+
+func _on_serious_delete_button_pressed(image_item, img_path: String):
+	var request = Requests.new();
+	var config = request.load_config();
+	
+	if (typeof(config) == TYPE_DICTIONARY): # error
+		create_notif_popup.emit("Failed to load config file.");
+		return;
+	
+	var global_path = ProjectSettings.globalize_path(img_path);
+	var error = OS.move_to_trash(global_path); # TODO check for errors
+	if (error != OK):
+		create_notif_popup.emit("Failed to delete file / File doesn't exist");
+		return;
+	
+	image_item.queue_free();
+
+
+func _on_copy_button_pressed(filename):
+	DisplayServer.clipboard_set(filename);
 
 # =====================
 # ====== Methods ======
@@ -63,32 +103,3 @@ func build_img_part(img_tex: ImageTexture, img_path: String):
 	delete_button.pressed.connect(_on_delete_button_pressed.bind(image_item, img_path));
 	
 	return image_item;
-
-
-func _on_delete_button_pressed(image_item, img_path: String):
-	create_action_popup.emit(
-		"Are you sure you want to delete this image?",
-		{ 'yes': "Delete Image", 'no': "Cancel" },
-		_on_serious_delete_button_pressed.bind(image_item, img_path) 
-	);
-
-
-func _on_serious_delete_button_pressed(image_item, img_path: String):
-	var request = Requests.new();
-	var config = request.load_config();
-	
-	if (typeof(config) == TYPE_DICTIONARY): # error
-		create_notif_popup.emit("Failed to load config file.");
-		return;
-	
-	var global_path = ProjectSettings.globalize_path(img_path);
-	var error = OS.move_to_trash(global_path); # TODO check for errors
-	if (error != OK):
-		create_notif_popup.emit("Failed to delete file / File doesn't exist");
-		return;
-	
-	image_item.queue_free();
-
-
-func _on_copy_button_pressed(filename):
-	DisplayServer.clipboard_set(filename);
